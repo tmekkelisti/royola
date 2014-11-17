69d285b7a2667e28992f06f253246c251393b14d
rohamo <rohamo@cs.helsinki.fi>
2014-11-17 23:45:05 +0200

    testiä että nousee herokuun2

--- src/main/java/wad/controller/UserController.java
+++ src/main/java/wad/controller/UserController.java
@@ -27,7 +27,7 @@ public class UserController {
     @RequestMapping(method = RequestMethod.POST)
     public String create(@ModelAttribute User user) {
         userRepo.save(user);
-        return "redirect:/login";
+        return "redirect:/index";
     }
 
 }
