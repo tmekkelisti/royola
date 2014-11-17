3183f60a7d71798682497dcff2c66a69319b2b69
Marko Laine <kheevo@gmail.com>
2014-11-17 15:11:01 +0200

    rikki on, signuppia tehdään

--- src/main/java/wad/controller/DefaultController.java
+++ src/main/java/wad/controller/DefaultController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import wad.domain.Story;
 import wad.service.StoryService;
+import wad.service.UserService;
 
 @Controller
 @RequestMapping("*")
@@ -17,6 +18,19 @@ public class DefaultController {
 
     @Autowired
     private StoryService storyService;
+    
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String viewLogin(Model model) {
+        return "login";
+    }
+
+    @RequestMapping(value = "signup", method = RequestMethod.GET)
+    public String viewSignup(Model model) {
+        return "signup";
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     public String view(Model model) throws UnsupportedEncodingException {
@@ -26,6 +40,8 @@ public class DefaultController {
             story.setContent(new String(iso, "UTF-8"));
             asd.add(story);
         }
+        
+//        model.addAttribute("user", userService.getAuthenticatedPerson().getUsername());
         model.addAttribute("stories", asd);
         return "index";
     }
---
+++ src/main/java/wad/controller/UserController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wad.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import wad.domain.User;
+import wad.repository.UserRepository;
+
+/**
+ *
+ * @author markolai
+ */
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String create(@ModelAttribute User user) {
+        userRepo.save(user);
+        return "redirect:/index";
+    }
+
+}
