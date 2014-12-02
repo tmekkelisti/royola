be5b1e41b43ec716bb322b61badf4a1182f9a358
Teemu Y <theme87@gmail.com>
2014-12-02 15:31:56 +0200

    domain testejä

--- src/test/java/wad/domain/CommentTest.java
+++ src/test/java/wad/domain/CommentTest.java
@@ -1,13 +1,51 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package wad.domain;
 
-/**
- *
- * @author unknown
- */
+import javax.annotation.PostConstruct;
+import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Comment.class)
 public class CommentTest {
     
+    private Comment comment;
+    
+    @PostConstruct
+    public void init() {
+        comment = new Comment();
+    }
+    
+    @Test
+    public void atStartNewCommentNull() {
+        Assert.assertEquals(null, comment.getBody());
+        assertNull(comment.getAuthor());
+        assertNull(comment.getStory());        
+    }
+    
+    @Test
+    public void atStartDateCreated(){
+        assertNotNull(comment.getCommentDate());
+    }
+        
+    @Test
+    public void setAuthorWorks(){
+        final String author = "author";
+        comment.setAuthor(author);
+        assertEquals(author, comment.getAuthor());
+    }
+    
+    @Test
+    public void setBodyWorks(){
+        final String commentBody = "comment body";
+        comment.setBody(commentBody);
+        assertEquals(commentBody, comment.getBody());
+    }
+
 }
--- src/test/java/wad/domain/PersonTest.java
+++ src/test/java/wad/domain/PersonTest.java
@@ -1,13 +1,50 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package wad.domain;
+
+
+import javax.annotation.PostConstruct;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  *
- * @author unknown
+ * @author teeyoshi
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Person.class)
 public class PersonTest {
+
+    private Person person;
+
+    @PostConstruct
+    public void init() {
+        person = new Person();
+    }
+
+    @Test
+    public void setUsernameWorks() {
+        final String username = "username";
+        person.setUsername(username);
+        assertEquals(username, person.getUsername());
+    }
+
+    @Test
+    public void setSaltWorks() {
+        final String salt = "salt";
+        person.setSalt(salt);
+        assertEquals(salt, person.getSalt());
+    }
+
+    @Test
+    public void passwordNotEncrypted() {
+        final String password = "password";
+        person.setPassword(password);
+        
+        assertNotEquals(password, person.getPassword());
+    }
     
+
 }
