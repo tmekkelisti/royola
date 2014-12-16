b23931e131e8395a4be60028dd270613d1a41e94
rohamo <rohamo@cs.helsinki.fi>
2014-12-16 22:59:28 +0200

    ääkkös settii

--- src/main/java/wad/controller/StoryController.java
+++ src/main/java/wad/controller/StoryController.java
@@ -39,21 +39,16 @@ public class StoryController {
 
     @RequestMapping(method = RequestMethod.POST)
     public String create(@Valid @ModelAttribute Story story, BindingResult br) {
-        // TODO: add person details in the future
-
         if (br.hasErrors()) {
             return "redirect:/index";
         }
-
         story.setContent(safeContent(story.getContent()));
         storyService.addStory(story);
-
         return "redirect:/index";
     }
 
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
     public String delete(@PathVariable Long id) {
-
         storyService.deleteStory(id);
         return "redirect:/index";
     }
@@ -70,47 +65,36 @@ public class StoryController {
         if (!storyService.getStory(id).getComments().isEmpty()) {
             model.addAttribute("comments", storyService.getStory(id).getComments());
         }
-
         return "singleStory";
     }
 
     @Transactional
     @RequestMapping(value = "/{id}", method = RequestMethod.POST)
     public String addComment(@PathVariable Long id, @Valid @ModelAttribute Comment comment, BindingResult br) {
-
         if (br.hasErrors()) {
 
             return "redirect:/stories/{id}";
         }
-
         comment.setBody(safeContent(comment.getBody()));
         storyService.addCommentToStory(comment, id);
-
         return "redirect:/stories/{id}#comments";
     }
 
     @Transactional
     @RequestMapping(value = "/{id}/vote", method = RequestMethod.POST)
     public String vote(@PathVariable Long id, @RequestParam Integer vote) {
-
         Story story = storyService.getStory(id);
         Integer voteCount = story.getVoteCount();
         voteCount += vote;
         story.setVoteCount(voteCount);
-
         return "redirect:/stories/{id}";
     }
 
-    @Transactional
     public String safeContent(String content) {
-
         String replaceAll = content.replaceAll("(\r\n|\n\r|\r|\n)", "<br />\n");
         Whitelist whitelist = new Whitelist();
         whitelist.addAttributes("br", "<br />\n");
         String safe = Jsoup.clean(replaceAll, whitelist);
-
         return safe;
-
     }
-
 }
