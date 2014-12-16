a3b34b53f8d9701dd812d156afff0fa859994863
rohamo <rohamo@cs.helsinki.fi>
2014-12-17 00:11:44 +0200

    ääkkös settii2

--- src/main/java/wad/config/SecurityConfiguration.java
+++ src/main/java/wad/config/SecurityConfiguration.java
@@ -2,12 +2,13 @@ package wad.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import wad.auth.JpaAuthenticationProvider;
 
 @Configuration
@@ -16,6 +17,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        http.addFilterBefore(filter,CsrfFilter.class);
         http.authorizeRequests()
                 .antMatchers("/login", "/signup", "/", "/index", "index","/get/**", "/stories/**", "/static*/**", "/users/**", "/best").permitAll()
                 .anyRequest().authenticated();
